3dad723329b3befae990a8f99857cbd4b824a0c2
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-07-04 15:22:38 +0200

    Scripts update

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -50,3 +50,44 @@ CREATE TABLE IF NOT EXISTS visits (
                                     description TEXT
 );
 CREATE INDEX ON visits (pet_id);
+
+-- Align with JPA schema
+
+ALTER TABLE vet_specialties
+  ADD CONSTRAINT pk_vet_specialties PRIMARY KEY (specialty_id, vet_id);
+
+ALTER TABLE owners
+  ALTER COLUMN address TYPE VARCHAR(255) USING (address::VARCHAR(255));
+
+ALTER TABLE owners
+  ALTER COLUMN city TYPE VARCHAR(255) USING (city::VARCHAR(255));
+
+ALTER TABLE visits
+  ALTER COLUMN description TYPE VARCHAR(255) USING (description::VARCHAR(255));
+
+ALTER TABLE owners
+  ALTER COLUMN first_name TYPE VARCHAR(255) USING (first_name::VARCHAR(255));
+
+ALTER TABLE vets
+  ALTER COLUMN first_name TYPE VARCHAR(255) USING (first_name::VARCHAR(255));
+
+ALTER TABLE owners
+  ALTER COLUMN last_name TYPE VARCHAR(255) USING (last_name::VARCHAR(255));
+
+ALTER TABLE vets
+  ALTER COLUMN last_name TYPE VARCHAR(255) USING (last_name::VARCHAR(255));
+
+ALTER TABLE pets
+  ALTER COLUMN name TYPE VARCHAR(255) USING (name::VARCHAR(255));
+
+ALTER TABLE specialties
+  ALTER COLUMN name TYPE VARCHAR(255) USING (name::VARCHAR(255));
+
+ALTER TABLE types
+  ALTER COLUMN name TYPE VARCHAR(255) USING (name::VARCHAR(255));
+
+ALTER TABLE owners
+  ALTER COLUMN telephone TYPE VARCHAR(255) USING (telephone::VARCHAR(255));
+
+ALTER TABLE pets
+  ALTER COLUMN type_id DROP NOT NULL;
