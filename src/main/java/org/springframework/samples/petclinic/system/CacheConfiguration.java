d2d1dd63095902a3c2dbfe4ec8230d507610cffe
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-12-03 18:47:03 +0100

    Minor improvements

--- src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
@@ -16,9 +16,7 @@
 
 package org.springframework.samples.petclinic.system;
 
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
@@ -26,10 +24,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
-import org.springframework.stereotype.Component;
 
 import javax.cache.configuration.MutableConfiguration;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -62,6 +58,7 @@ class CacheConfiguration {
 	}
 
 	@Bean
+	@ConditionalOnProperty(name = "notifications.engine", havingValue = "prod")
 	public BeanFactoryPostProcessor preventProductionService() {
 		return beanFactory -> {
 			ConfigurableEnvironment env = beanFactory.getBean(ConfigurableEnvironment.class);
