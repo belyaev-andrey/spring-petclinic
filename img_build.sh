33716bbf4594a9194fa0aa26cbcf439295dd350a
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-12-06 18:52:25 +0100

    Added image build and sh file

---
+++ img_build.sh
@@ -0,0 +1,5 @@
+docker buildx build --platform linux/amd64 -t app-petclinic-image:0.1-$(date +%s%3) -f images/Dockerfile .
+
+/usr/local/bin/docker compose --platform linux/amd64 -f docker-compose.yml -p spring-petclinic up -d app
+
+
