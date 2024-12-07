70279c44dfdaa67fcb3d87272451d778f194580e
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-12-07 21:14:32 +0100

    Using ARM platform temporarily

--- img_build.sh
+++ img_build.sh
@@ -1,5 +1,5 @@
+#!/usr/bin/env sh
 docker buildx build --platform linux/amd64 -t app-petclinic-image:0.1-$(date +%s%3) -f images/Dockerfile .
-
 /usr/local/bin/docker compose --platform linux/amd64 -f docker-compose.yml -p spring-petclinic up -d app
 
 
