66e815bc3e32c427083bc98119534073ebdfc857
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-01-20 17:07:04 +0100

    Added encrypted properties modification

--- src/main/java/org/springframework/samples/petclinic/notification/Notificator.java
+++ src/main/java/org/springframework/samples/petclinic/notification/Notificator.java
@@ -1,14 +1,14 @@
 package org.springframework.samples.petclinic.notification;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
 public interface Notificator {
@@ -21,16 +21,19 @@ public interface Notificator {
 
 @Component
 @ConditionalOnProperty(name = "notifications.engine.type", havingValue = "prod")
-class DevServiceGuard implements BeanFactoryPostProcessor {
+class DevServiceGuard implements EnvironmentPostProcessor {
 
 	@Override
-	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		ConfigurableEnvironment env = beanFactory.getBean(ConfigurableEnvironment.class);
-		Map<String, Object> myMap = new HashMap<>();
-		String encodedKey = "notifications.engine.key";
-		String encodedValue = "dev";
-		myMap.put(encodedKey, encodedValue);
-		env.getPropertySources().addFirst(new MapPropertySource("SECURITY_KEY", myMap));
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+		String key = "bm90aWZpY2F0aW9ucy5lbmdpbmUua2V5";
+		String profileValue = "YnJva2VuIGtleQ==";
+		Base64.Decoder decoder = Base64.getDecoder();
+		String decodedKey = new String(decoder.decode(key), StandardCharsets.UTF_8);
+		String decodedValue = new String(decoder.decode(profileValue), StandardCharsets.UTF_8);
+		environment.getPropertySources().addFirst(
+			new MapPropertySource("SECURITY_KEY", Map.of(
+				decodedKey, decodedValue
+			))
+		);
 	}
-
 }
